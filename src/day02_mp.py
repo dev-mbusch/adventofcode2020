9e5160923d2093059575c0024fd37ab32843cd55
mapfa <marten_pf@web.de>
2020-12-03 00:00:58 +0100

    day 2 part 1 mp

---
+++ src/day02_mp.py
@@ -0,0 +1,41 @@
+import re
+
+def lineparser (policy_and_password: str):
+    """Takes in a string consisting of a policy followed by a password.
+    Policies define min and max occurencies of a single character within a password, e.g. min-max x.
+    Policies and passwords are seperated by a colon.
+    An example input string looks like 2-7 x: abcxyz"""
+
+    policy = re.split(r':', policy_and_password)[0]
+    password = re.split(r':', policy_and_password)[1].strip()
+
+    patterns = re.match("(\d+)-(\d+)\s(\w)", policy).groups()
+
+    return (*patterns, password)
+
+def password_validator(min, max, magic_char, password):
+    """Takes in elements of a policy: min and mac occurency of a specific chareacter as well as a password to be validated against the policy."""
+    magic_char_count = password.count(magic_char)
+    is_valid = (magic_char_count >= int(min) and magic_char_count <= int(max))
+    return is_valid
+
+
+#sample_input = "4-5 t: ttglwxxghtznp"
+# parsed_input = lineparser(sample_input)
+# #print(password_validator(1,12,"t","ttglwxxghtznp"))
+#print(password_validator(*lineparser(sample_input)))
+
+
+if __name__ == '__main__':
+    file = open("input_files\day02_input_mp.txt")
+    lines = file.read().split("\n")
+    file.close()
+
+    # define counter
+    valid_password_count = 0
+
+    for line in lines:
+        if password_validator(*lineparser(line)):
+            valid_password_count += 1
+    
+    print(valid_password_count)
