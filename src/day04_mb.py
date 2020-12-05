20f4e18edd07ae2f08efe7f2ff3e3935545a3c03
dev-mbusch <dev.mbusch@gmail.com>
2020-12-05 06:05:43 +0100

    Add Solution Day04 Part One MB

--- src/day04_mb.py
+++ src/day04_mb.py
@@ -2,95 +2,36 @@
 https://adventofcode.com/2020/day/4
 """
 
-import re
-
 # Read Input File
 with open(r'../input_files/day04_input_mb.txt', 'r') as fh:
-    lines = [line.strip('\n') for line in fh.readlines()]
+    raw_input = fh.read().split('\n\n')
+    passports = [line.replace('\n', ' ') for line in raw_input]
 
-def process_data(lines):
-    """Processes the input file and generates a list of individual passports."""
-    passport = []
-    passports = []
-    for item in lines:
-        if item == '':
-            temp = passport[:]
-            passport.clear()
-            passports.append(temp)
-            continue
-        else:
-            passport.append(item)
+requirements = [
+    'byr',
+    'iyr',
+    'eyr',
+    'hgt',
+    'hcl',
+    'ecl',
+    'pid',
+    # 'cid'
+]
 
-    return passports
+def check_passport_validity(passports, required_fields):
+    valid_passports = 0
+    for i, passport in enumerate(passports):
+        requirements_met = 0
+        # print(f'{i = } {passport = }')
+        for j, field in enumerate(required_fields):
+            # print(f'{j = } {field = }')
+            if field in passport:
+                requirements_met += 1
+                # print(f'{requirements_met = }')
+        if requirements_met == 7:
+            valid_passports += 1
+            # print(f'{valid_passports =}')
+    return valid_passports
 
-def create_passport_records(passport_lines):
-    """Takes a list of passport lines and creates a list of concatenated passports."""
-    passporttexts = []
-
-    for passport in passport_lines:
-        pp_text = ''
-        for i, line in enumerate(passport):
-            if i != len(passport):
-                pp_text += line + ' '
-            else:
-                pp_text += line
-
-        passporttexts.append(pp_text)
-        # print(pp_text)
-    return passporttexts
-
-def check_passport_validity(passport):
-    """Checks the validity of the passport."""
-    counter = 0
-
-    fields = passport.split(' ')
-
-    fields = [x.strip() for x in fields]
-
-    if '' in fields:
-        fields.remove('')
-
-    # fields_set = set(fields)
-
-    # if ('cid:' in fields_set and len(fields_set) == 7) or ('cid:' not in fields_set and len(fields_set) > 7):
-    #     return True
-    # else:
-    #     return False
-    # # return fields
-    # # return len(fields)
-
-    validity = {
-        'byr:',
-        'iyr:',
-        'eyr:',
-        'hgt:',
-        'hcl:',
-        'ecl:',
-        'pid:',
-        # 'cid:',
-    }
-
-
-
-    for field in validity:
-        if field in passport or field == 'cid:':
-            counter += 1
-
-    if counter >= 7:
-        return True
-    else:
-        return False
-
-
-# Use to Generate List of Passports
-passport_lines = process_data(lines)
-passports = create_passport_records(passport_lines)
-
-# for passport in passports:
-#     print(check_passport_validity(passport))
-
-valid_passports = 0
-for passport in passports:
-    if check_passport_validity(passport):
-        valid_passports += 1
-print(valid_passports)
+# Part One
+print(f'Part one - Number of valid passports: {check_passport_validity(passports, requirements)}')
